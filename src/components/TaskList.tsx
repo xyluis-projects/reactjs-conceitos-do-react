bced74df98c34a1fd205058381b34bcf5d32ea85
Luís Felipe <luisfelipe.assuncao@hotmail.com>
2022-05-29 19:35:15 -0400

    feat: challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,25 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle || newTaskTitle.length <= 0 || newTaskTitle === '') return
+
+    const task: Task = {
+      id: Math.random(),
+      isComplete: false,
+      title: newTaskTitle
+    }
+
+    setTasks(current => [...current, task])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => task.id === id ? { ...task, isComplete: !task.isComplete } : task))
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id))
   }
 
   return (
